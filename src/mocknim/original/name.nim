a367c3062fa49b84d7ff6e411b307edccf9780ae
Alexander Novikov <mem-memov@yandex.ru>
2019-01-01 23:04:31 +0300

    coding

--- src/mocknim/original/name.nim
+++
@@ -1,9 +0,0 @@
-type
-  Name* = ref object
-    node: NimNode
-
-proc newName*(node: NimNode): Name = 
-
-  Name(
-    node: node
-  )
--- src/mocknim/original/procedureOriginal.nim
+++ src/mocknim/original/procedureOriginal.nim
@@ -5,18 +5,12 @@ import
   mocknim/[
     original/typeOriginals,
     original/argumentOriginal,
-    original/argumentOriginals,
-    original/name,
-    original/self,
-    original/arguments
+    original/argumentOriginals
   ]
 
 
 type
   ProcedureOriginal* = ref object
-    name: Name
-    self: Self
-    arguments: Arguments
     typeOriginals: TypeOriginals
     argumentOriginals: ArgumentOriginals
 
@@ -33,9 +27,6 @@ proc newProcedureOriginal*(procDefNode: NimNode, moduleName: string): ProcedureO
   let formalParamsNode = procDefNode[3]
 
   ProcedureOriginal(
-    name: newName(procDefNode),
-    self: newSelf(procDefNode, moduleTypeName),
-    arguments: newArguments(procDefNode),
     typeOriginals: newTypeOriginals(formalParamsNode, moduleTypeName),
     argumentOriginals: newArgumentOriginals(formalParamsNode)
   )
@@ -54,71 +45,3 @@ proc moduleTypeName*(this: ProcedureOriginal): string =
 proc arguments*(this: ProcedureOriginal): seq[ArgumentOriginal] =
 
   this.argumentOriginals.create()
-
-
-# proc mock*(this: ProcedureOriginal): NimNode =
-
-#   result = newStmtList()
-
-  # let selfExists = this.self.exists()
-
-  # if this.arguments.exist(selfExists):
-  #   this.arguments.mockTypes(selfExists)
-
-  # if selfExists:
-  #   echo "eeeeeeeeeee"
-
-#   var allTypes: seq[string] = @[]
-
-#   let resultType = this.node[3][0].repr()
-#   allTypes.add(resultType)
-
-#   for i, argument in this.node[3]:
-
-#     if i == 0:
-#       continue
-
-#     let argumentType = argument[0].repr()
-#     allTypes.add(argumentType)
-
-#   let uniquetypes = allTypes.deduplicate()
-
-#   let types = newTypes(uniquetypes)
-
-#   var statements: seq[NimNode] = @[]
-  
-#   statements.add(types.mock(this.node))
-
-#   # echo this.node.repr()
-#   # echo this.node.treeRepr()
-
-#   let procedureName = this.node[0][1].repr()
-
-#   let definition = newTree(nnkProcDef,
-#     newIdentNode(procedureName),
-#     newEmptyNode(),
-#     newEmptyNode(),
-#     newTree(nnkFormalParams,
-#       newEmptyNode(),
-#     ),
-#     newEmptyNode(),
-#     newEmptyNode(),
-#     newTree(nnkStmtList,
-#       newTree(nnkCommand,
-#         newIdentNode("echo"),
-#         newStrLitNode("Anonymous")
-#       )
-#     )
-#   )
-
-#   # echo definition.repr()
-
-#   statements.add(definition)
-
-#   result = newStmtList(statements)
-
-#   # echo result.treeRepr()
-
-# # dumpTree:
-# #   proc myfn() =
-# #     echo "Anonymous"
