ae93cfa9ee98282ee69f916d64d788ffbbc3fb4a
Alexander Novikov <mem-memov@yandex.ru>
2019-01-03 01:15:02 +0300

    copy module

--- src/mocknim/original/moduleOriginal.nim
+++ src/mocknim/original/moduleOriginal.nim
@@ -6,13 +6,17 @@ import
 
 type
   ModuleOriginal* = ref object
+    statementsNode: NimNode
     dependencyOriginals: seq[DependencyOriginal]
     name: string
 
 
 proc newModuleOriginal*(statementsNode: NimNode, name: string, dependencyOriginals: seq[DependencyOriginal]): ModuleOriginal =
 
+  expectKind(statementsNode, nnkStmtList)
+
   ModuleOriginal(
+    statementsNode: statementsNode,
     dependencyOriginals: dependencyOriginals,
     name: name
   )
@@ -21,3 +25,10 @@ proc newModuleOriginal*(statementsNode: NimNode, name: string, dependencyOrigina
 proc dependencies*(this: ModuleOriginal): seq[DependencyOriginal] =
 
   this.dependencyOriginals
+
+
+proc copyWithoutImportStatement(this: ModuleOriginal): NimNode =
+
+  result = this.statementsNode.copyNimTree()
+
+  echo result.treeRepr()
